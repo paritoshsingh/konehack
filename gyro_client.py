be63349b9b7b55a438b58e519004f6c318ee35af
paari <paritoshsinghrulz@gmail.com>
2017-02-23 21:44:11 +0530

    gyro

--- gyro_client.py
+++ gyro_client.py
@@ -2,13 +2,54 @@ import time
 import paho.mqtt.client as mqtt
 import json
 import uuid
+import smbus
+import math
 
 
 
-deviceId="gyro-pi1" #you can give the mac address as default also
+# Power management registers
+power_mgmt_1 = 0x6b
+power_mgmt_2 = 0x6c
+
+def read_byte(adr):
+    return bus.read_byte_data(address, adr)
+
+def read_word(adr):
+    high = bus.read_byte_data(address, adr)
+    low = bus.read_byte_data(address, adr+1)
+    val = (high << 8) + low
+    return val
+
+def read_word_2c(adr):
+    val = read_word(adr)
+    if (val >= 0x8000):
+        return -((65535 - val) + 1)
+    else:
+        return val
+
+def dist(a,b):
+    return math.sqrt((a*a)+(b*b))
+
+def get_y_rotation(x,y,z):
+    radians = math.atan2(x, dist(y,z))
+    return -math.degrees(radians)
+
+def get_x_rotation(x,y,z):
+    radians = math.atan2(y, dist(x,z))
+    return math.degrees(radians)
+
+
+bus = smbus.SMBus(1) # or bus = smbus.SMBus(1) for Revision 2 boards
+address = 0x68       # This is the address value read via the i2cdetect command
+
+# Now wake the 6050 up as it starts in sleep mode
+bus.write_byte_data(address, power_mgmt_1, 0)
+
+
 
 #Set the variables for connecting to the iot service
 broker = ""
+deviceId="gyro-pi1" #you can give the 	 address as default also
 topic = "iot-2/evt/status/fmt/json"
 username = "use-token-auth"
 password = "gyropiauth" #auth-token
@@ -38,10 +79,17 @@ mqttc.connect(host=broker, port=1883, keepalive=60)
 mqttc.loop_start() 
 
 while mqttc.loop() == 0:
-	cpuutilvalue = 11
-	print cpuutilvalue
+	gyro_xout = read_word_2c(0x43)
+	gyro_yout = read_word_2c(0x45)
+	gyro_zout = read_word_2c(0x47)
+	accel_xout = read_word_2c(0x3b)
+	accel_yout = read_word_2c(0x3d)
+	accel_zout = read_word_2c(0x3f)
+	accel_xout_scaled = accel_xout / 16384.0
+	accel_yout_scaled = accel_yout / 16384.0
+	accel_zout_scaled = accel_zout / 16384.0
 
- 	msg = json.JSONEncoder().encode({"aaaa":{"blah":cpuutilvalue}})
+ 	msg = json.JSONEncoder().encode({"d":{"gyro_xout":gyro_xout, "gyro_yout":gyro_yout, "gyro_zout":gyro_zout, "accel_xout":accel_xout, "accel_yout":accel_yout, "accel_zout":accel_zout}})
  
  	mqttc.publish(topic, payload=msg, qos=0, retain=False)
  	print "message published"

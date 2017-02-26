b9332a9eab858f1e0bd1bd1075a7ddef9a43dca3
paari <paritoshsinghrulz@gmail.com>
2017-02-26 04:31:21 +0000

    final commits

--- send_mq_msg.py
+++ send_mq_msg.py
@@ -7,7 +7,9 @@ from datetime import datetime
 import sqlite3
 
 #Set the variables for connecting to the iot service
+msg= ""
 broker = ""
+r = []
 deviceId="gyro-pi1" #you can give the 	 address as default also
 topic = "iot-2/evt/status/fmt/json"
 username = "use-token-auth"
@@ -20,28 +22,36 @@ topic = "iot-2/evt/status/fmt/json"
 clientID = "d:" + organization + ":" + deviceType + ":" + deviceId
 broker = organization + ".messaging.internetofthings.ibmcloud.com"
 mqttc = mqtt.Client(clientID)
+conn=sqlite3.connect("/home/pi/sqlite3/msgDb.db")
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     try:
-    	conn=sqlite3.connect("/home/pi/sqlite3/msgDb.db")
-		c = conn.cursor()
-		c.execute("select a.* from messages a left join sent_messages b on a.message_id=b.message_id and b.message_id is null order by a.message_id desc limit 10;")
-		r = [dict((c.description[i][0], value) \
-			for i, value in enumerate(row)) for row in c.fetchall()]
-		
-		# print json.dumps(r[0])	
- 		msg = json.JSONEncoder().encode({"d":r[0]})
- 		# json.dumps(msg)
- 	except Exception as e:
- 		print e
- 		conn.close()
+	c = conn.cursor()
+	#c.execute("select a.* from messages a left join sent_messages b on a.message_id=b.message_id and b.message_id is null order by a.message_id asc limit 1;")
+	c.execute("select * from messages limit 1")
+	r = [dict((c.description[i][0], value) \
+		for i, value in enumerate(row)) for row in c.fetchall()]
+	print "RRR"
+	print r[0]		
+	# print json.dumps(r[0])	
+ 	msg = json.JSONEncoder().encode({"d":r[0]})
+    except Exception as e:
+ 	print e
+ 	conn.close()
 
 def on_publish(mosq, obj, mid):
-	for item in r:
- 		c.execute("insert into sent_messages values (?);", item['message_id'])
- 	
+	print "looking "
+	print mid
+	try:	
+	    c=conn.cursor()
+	    c.execute("delete from messages where message_id =(select message_id from sent_messages where mid=?);", mid)
+	    c.commit()
+	except Exception as e:
+	    print e
+	    conn.close()
+ 	print "sent"
 
 ##Assign callbacks
 mqttc.on_connect = on_connect
@@ -56,11 +66,16 @@ mqttc.connect(host=broker, port=1883, keepalive=60)
 #Publishing to IBM Internet of Things Foundation
 mqttc.loop_start() 
 
-while mqttc.loop() == 0:
-	
-	# mqttc.publish(topic, payload=msg, qos=2, retain=False)
+try:
+    pub=mqttc.publish(topic, payload=msg, qos=1, retain=False)
+    print "mid pubbed"
+    print pub.mid
+    c=conn.cursor()
+    c.execute("insert into sent_messages (message_id, mid) values (?,?)", (1,pub['mid']))
+except Exception as e:
+    print e	
+mqttc.loop_forever()
  	# print "message published"
- 	publish.multiple()
- 	pass
+ 	# publish.multiple(r, hostname=broker, port=1883, client_id=clientID, keepalive=60)
 
 

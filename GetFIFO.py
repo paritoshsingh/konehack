405b80b2ab688751403310a070dddf974c5bdfb8
Paritosh Singh <Paritosh.Singh@quaero.com>
2017-03-01 15:41:49 +0530

    ready to test

--- GetFIFO.py
+++ GetFIFO.py
@@ -8,8 +8,8 @@ import numpy
 #TargetSampleNumber= 1024
 #TargetRate =  33    # frequency =  8000 / ( integr value + 1)  minimum frequency=32,25
 
-InputSampleRate=raw_input("Sample Rate(32.25 ... 2000) ?")
-InputSampleNumber=raw_input("Number of sample to take ?")    
+InputSampleRate = 33#raw_input("Sample Rate(32.25 ... 2000) ?")
+InputSampleNumber = 1024 #raw_input("Number of sample to take ?")    
 
 TargetSampleNumber= int(InputSampleNumber)
 TargetRate= float(InputSampleRate)
@@ -57,52 +57,5 @@ while True:
  # print Total
  if Total >= TargetSampleNumber :
    break;
-
- #now that we have the data let's write the files
-
-
-if Total > 0:
-  Status = mpu6050.readStatus()
-  # print "Status",Status
-  if (Status & 0x10) == 0x10 :
-    print "Overrun Error! Quitting.\n"
-    quit()
-
-  print "Saving RawData.txt  file."
-  
-  FO = open("RawData.txt","w")
-  FO.write("GT\tGx\tGy\tGz\tTemperature\tGyrox\tGyroy\tGyroz\n")
-  fftdata = []
-  for loop in range (TargetSampleNumber):
-    SimpleSample = Values[loop*14 : loop*14+14]
-    I = mpu6050.convertData(SimpleSample)
-    CurrentForce = math.sqrt( (I.Gx * I.Gx) + (I.Gy * I.Gy) +(I.Gz * I.Gz))
-    fftdata.append(CurrentForce)
-    FO.write("{0:6.3f}\t{1:6.3f}\t{2:6.3f}\t{3:6.3f}\t".format(CurrentForce, I.Gx , I.Gy, I.Gz))
-    FO.write("{0:5.1f}\t{1:6.3f}\t{2:6.3f}\t{3:6.3f}\n".format(I.Temperature,I.Gyrox,I.Gyroy,I.Gyroz))
-
-  FO.close()
-
-  print "Calculate FFT"
-
-  fourier = numpy.fft.fft(fftdata)
-
-  print "Save FFTData.txt file"
-  FO = open("FFTData.txt","w")
-  fftData = numpy.abs(fourier[0:len(fourier)/2+1])/TargetSampleNumber
-  frequency = []
-  FO.write( "Frequency\tFFT\n")
-  Peak=0
-  PeakIndex=0;
-  for loop in range(TargetSampleNumber/2 +1):
-    frequency.append( loop * TargetRate/ TargetSampleNumber)
-    FO.write("{0}\t{1}\n".format(frequency[loop],fftData[loop]))
-    if loop>0:
-       if fftData[loop] > Peak :
-         Peak=fftData[loop]
-         PeakIndex=loop
-
-  print "Peak at {0}Hz = {1}".format(frequency[PeakIndex],Peak)
-   
-
-print "Done!"
+print Values
+ 

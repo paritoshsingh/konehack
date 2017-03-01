5c0c8f12454593381671dbcafbf047b68c4caabf
paari <paritoshsinghrulz@gmail.com>
2017-03-01 21:31:11 +0000

    fixed incorrect [loop] reference

--- GetFIFO.py
+++ GetFIFO.py
@@ -89,16 +89,20 @@ while mqttc.loop() == 0:
   for loop in range (TargetSampleNumber):
     SimpleSample = Values[loop*14 : loop*14+14]
     I = mpu6050.convertData(SimpleSample)
-    gyro_xout_scaled =(I.Gyrox[loop]/131)
-    gyro_yout_scaled =(I.Gyroy[loop]/131)
-    gyro_zout_scaled =(I.Gyroz[loop]/131)
+    print type(I)
+    print type(I.Gx)
+    print I.Gx
+    print I
+    gyro_xout_scaled =(I.Gyrox/131)
+    gyro_yout_scaled =(I.Gyroy/131)
+    gyro_zout_scaled =(I.Gyroz/131)
 
-    accel_xout = I.Gx[loop]
-    accel_yout = I.Gy[loop]
-    accel_zout = I.Gz[loop]
-    accel_xout_scaled = accel_xout / 16384.0
-    accel_yout_scaled = accel_yout / 16384.0
-    accel_zout_scaled = accel_zout / 16384.0
+    accel_xout = I.Gx
+    accel_yout = I.Gy
+    accel_zout = I.Gz
+    accel_xout_scaled = accel_xout/16384.0
+    accel_yout_scaled = accel_yout/16384.0
+    accel_zout_scaled = accel_zout/16384.0
 
     x_rotation=get_x_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
     y_rotation=get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
